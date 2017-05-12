0dfa337f7f747acdd1e1ba2e1ea7649d52adf831
maxl <maxl@mr-finch.lamboz.priv>
2017-05-12 17:48:28 +0200

    Added actionButton

---
+++ src/widgets/actionbutton.cpp
@@ -0,0 +1,6 @@
+#include "actionbutton.h"
+
+ActionButton::ActionButton()
+{
+
+}
---
+++ src/widgets/actionbutton.h
@@ -0,0 +1,13 @@
+#ifndef ACTIONBUTTON_H
+#define ACTIONBUTTON_H
+
+#include <QObject>
+#include <QWidget>
+
+class ActionButton : public QPushButton
+{
+public:
+    ActionButton();
+};
+
+#endif // ACTIONBUTTON_H
