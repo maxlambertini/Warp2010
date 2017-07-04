fbae0d4a121a09f2639d0fcd73f3cb5f320230f3
Masssimiliano Lambertini <m.lambertini@keposnet.com>
2017-07-04 14:14:18 +0200

    Building editor....

--- jstexture/texturebuilderwidget.cpp
+++ jstexture/texturebuilderwidget.cpp
@@ -1,80 +1,124 @@
 #include "texturebuilderwidget.h"
 #include <QDialogButtonBox>
+#include <QIntValidator>
+#include <QDoubleValidator>
 
 TextureBuilderWidget::TextureBuilderWidget(QWidget *parent) : QWidget(parent)
 {
-    int nRow = 0;
-    QGridLayout* grid = new QGridLayout(this);
+    createWidgets();
+    layoutWidgets();
+}
 
-    QVBoxLayout *l1 = new QVBoxLayout (this);
-    QVBoxLayout *l2 = new QVBoxLayout (this);
+void TextureBuilderWidget::createWidgets() {
 
-    grid->addWidget(new QLabel("Size",this),nRow,0);
+    QIntValidator *v1 = new QIntValidator(-90,90,this);
+    QIntValidator *v2 = new QIntValidator(-180,180,this);
+    QIntValidator *v = new QIntValidator(this);
 
     c_sizeX = new QLineEdit(this);
-    l2->addWidget(new QLabel("X"));
-    l2->addWidget(c_sizeX);
-
-    l2->addWidget(new QLabel("sizeY",this),nRow,0);
+    c_sizeX->setValidator(v);
     c_sizeY = new QLineEdit(this);
-    l2->addWidget(c_sizeY);
-
-    grid->addLayout(l2,nRow, 1);
-
-    ++nRow;
-
-    grid->addWidget(new QLabel("Sphere Range",this),nRow,0);
-
-    l1->addWidget(new QLabel("N"));
+    c_sizeY->setValidator(v);
     c_north = new QLineEdit(this);
-    l1->addWidget(c_north);
-
-    l1->addWidget(new QLabel("S"));
+    c_north->setValidator(v1);
     c_south = new QLineEdit(this);
-    l1->addWidget(c_south);
-
-    l1->addWidget(new QLabel("W"));
+    c_south->setValidator(v1);
     c_west = new QLineEdit(this);
-    l1->addWidget(c_west);
-
-    l1->addWidget(new QLabel("E"));
+    c_west->setValidator(v2);
     c_east = new QLineEdit(this);
-    l1->addWidget(c_east);
-
-    grid->addLayout(l1,nRow,1);
-
-
-    ++nRow;
-    grid->addWidget(new QLabel("builderType",this),nRow,0);
+    c_east->setValidator(v2);
     c_builderType = new QComboBox(this);
     QStringList lst; lst << "sphere" << "rectangle" << "cylinder";
     c_builderType->addItems(lst);
+    c_builderType->setCurrentText("sphere");
+    c_bumpMap = new QLineEdit(this);
+    c_cloudMap = new QLineEdit(this);
+    c_colorMap = new QLineEdit(this);
+    c_reflectionMap = new QLineEdit(this);
+    c_randomFactors = new QLineEdit(this);
+    c_randomFactors->setMinimumWidth(300);
+
+}
+
+void TextureBuilderWidget::layoutWidgets() {
+
+    int nRow = 0;
+    QGridLayout* grid = new QGridLayout(this);
+    QVBoxLayout *l1 = new QVBoxLayout (this);
+    QVBoxLayout *l2 = new QVBoxLayout (this);
+    grid->addWidget(new QLabel("Size",this),nRow,0);
+    l2->addWidget(new QLabel("X"));
+    l2->addWidget(c_sizeX);
+    l2->addWidget(new QLabel("sizeY",this),nRow,0);
+    l2->addWidget(c_sizeY);
+    grid->addLayout(l2,nRow, 1);
+
+    ++nRow;
+    grid->addWidget(new QLabel("Sphere Range",this),nRow,0);
+    l1->addWidget(new QLabel("N"));
+    l1->addWidget(c_north);
+    l1->addWidget(new QLabel("S"));
+    l1->addWidget(c_south);
+    l1->addWidget(new QLabel("W"));
+    l1->addWidget(c_west);
+    l1->addWidget(new QLabel("E"));
+    l1->addWidget(c_east);
+    grid->addLayout(l1,nRow,1);
+
+    ++nRow;
+    grid->addWidget(new QLabel("builderType",this),nRow,0);
     grid->addWidget(c_builderType,nRow,1);
 
     ++nRow;
     grid->addWidget(new QLabel("bumpMap",this),nRow,0);
-    c_bumpMap = new QComboBox(this);
     grid->addWidget(c_bumpMap,nRow,1);
 
     ++nRow;
     grid->addWidget(new QLabel("cloudMap",this),nRow,0);
-    c_cloudMap = new QComboBox(this);
     grid->addWidget(c_cloudMap,nRow,1);
 
     ++nRow;
     grid->addWidget(new QLabel("colorMap",this),nRow,0);
-    c_colorMap = new QComboBox(this);
     grid->addWidget(c_colorMap,nRow,1);
 
     ++nRow;
     grid->addWidget(new QLabel("reflectionMap",this),nRow,0);
-    c_reflectionMap = new QLineEdit(this);
     grid->addWidget(c_reflectionMap,nRow,1);
-
 
     ++nRow;
     grid->addWidget(new QLabel("randomFactors",this),nRow,0);
-    c_randomFactors = new QLineEdit(this);
     grid->addWidget(c_randomFactors,nRow,1);
+
+
+}
+
+void TextureBuilderWidget::readFromTextureBuilder() {
+    auto b = _tb->bounds();
+    c_north->setText(QString("%1").arg(std::get<0>(b)));
+    c_south->setText(QString("%1").arg(std::get<1>(b)));
+    c_west->setText(QString("%1").arg(std::get<2>(b)));
+    c_east->setText(QString("%1").arg(std::get<3>(b)));
+    auto s = _tb->size();
+    c_sizeX->setText(QString("%1").arg(std::get<0>(s)));
+    c_sizeY->setText(QString("%1").arg(std::get<1>(s)));
+
+    c_bumpMap->setText(_tb->bumpMap());
+    c_colorMap->setText(_tb->colorMap());
+    c_cloudMap->setText(_tb->cloudMap());
+    c_reflectionMap->setText(_tb->reflectionMap());
+
+    if (_tb->randomFactors().count() > 0) {
+        QStringList lst;
+        for (auto i = _tb->randomFactors().begin(); i != _tb->randomFactors().end(); ++i) {
+            auto value = *i;
+            lst << QString("%1").arg(value);
+        }
+        QString data = lst.join(", ");
+        c_randomFactors->setText(data);
+    }
+
+}
+
+void TextureBuilderWidget::writeToTextureBuilder() {
 
 }
--- jstexture/texturebuilderwidget.h
+++ jstexture/texturebuilderwidget.h
@@ -11,27 +11,37 @@
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QButtonGroup>
+#include <QComboBox>
+#include <texturebuilder/texturebuilder.h>
 
 class TextureBuilderWidget : public QWidget
 {
     Q_OBJECT
-public:
-    explicit TextureBuilderWidget(QWidget *parent = 0);
 
     QLineEdit* c_north;
     QLineEdit* c_south;
     QLineEdit* c_west;
     QLineEdit* c_east;
     QComboBox* c_builderType;
-    QComboBox* c_bumpMap;
-    QComboBox* c_cloudMap;
-    QComboBox* c_colorMap;
+    QLineEdit* c_bumpMap;
+    QLineEdit* c_cloudMap;
+    QLineEdit* c_colorMap;
     QLineEdit* c_reflectionMap;
     QLineEdit* c_randomFactors;
     QLineEdit* c_sizeX;
     QLineEdit* c_sizeY;
 
+    TextureBuilder *_tb;
+public:
+    explicit TextureBuilderWidget(QWidget *parent = 0);
     void createWidgets();
+    void layoutWidgets();
+
+    TextureBuilder *tb() { return _tb; }
+    void setTextureBuilder(TextureBuilder *tb) { _tb = tb; }
+
+    void readFromTextureBuilder();
+    void writeToTextureBuilder();
 
 signals:
 
